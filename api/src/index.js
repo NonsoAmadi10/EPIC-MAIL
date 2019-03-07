8d04a28837a1e588e5fbb3bb63bb7ca3eee04467
Nonso Amadi <nonsoamadi@aol.com>
2019-03-07 11:49:00 +0100

    (chore): Test is passing on first conditional

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import userRoutes from './routes/user.route';
+
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use('/api/v1', userRoutes);
 
 app.get('/', (req, res) => {
   res.json({
