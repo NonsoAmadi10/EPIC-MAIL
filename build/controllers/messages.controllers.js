f33b2966027f141df0a322afd174c5968a5f8bee
Nonso Amadi <nonsoamadi@aol.com>
2019-03-12 10:22:27 +0100

    fixes(endpoint): Re configure travis - Remove the 'cd api/.' command from .travis.yml - npm run test locally - Test is Passing
    [Delevers #164565211]

---
+++ build/controllers/messages.controllers.js
@@ -0,0 +1,90 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _messages = _interopRequireDefault(require("../services/messages.service"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var messagesController = {
+  fetchAllUnreadMessage: function fetchAllUnreadMessage(req, res) {
+    var allUnreadMessages = _messages.default.getallUnreadMessages();
+
+    return res.json({
+      status: 200,
+      data: allUnreadMessages
+    });
+  },
+  fetchAllMessages: function fetchAllMessages(req, res) {
+    var allMessages = _messages.default.getAllReceivedMessages();
+
+    return res.json({
+      status: 200,
+      data: allMessages
+    });
+  },
+  fetchAllSentMessages: function fetchAllSentMessages(req, res) {
+    var sentMessages = _messages.default.getAllSentMessages();
+
+    return res.json({
+      status: 200,
+      data: sentMessages
+    });
+  },
+  fetchSingleMessage: function fetchSingleMessage(req, res) {
+    var id = Number(req.params.id);
+
+    var found = _messages.default.getASingleMessage(id);
+
+    if (!found) {
+      return res.json({
+        status: 401,
+        error: 'Bad Request'
+      });
+    }
+
+    return res.json({
+      status: 200,
+      data: found
+    });
+  },
+  postMessages: function postMessages(req, res) {
+    var messageBody = req.body;
+
+    if (messageBody.subject === ' ' || messageBody.message === ' ') {
+      return res.json({
+        status: 404,
+        error: 'empty input body'
+      });
+    }
+
+    var newMessage = _messages.default.postMessages(req.body);
+
+    return res.json({
+      status: 200,
+      data: newMessage
+    });
+  },
+  deleteMessagesController: function deleteMessagesController(req, res) {
+    var id = Number(req.params.id);
+
+    var deleteMessage = _messages.default.deleteMessages(id);
+
+    if (!deleteMessage) {
+      res.json({
+        status: 404,
+        error: 'Invalid ID'
+      });
+    }
+
+    return res.json({
+      status: 200,
+      message: 'message deleted'
+    });
+  }
+};
+var _default = messagesController;
+exports.default = _default;
